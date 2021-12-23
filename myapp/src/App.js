05566c8fdd600e4266996c938aa9651c1337eeb0
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2021-12-23 16:50:46 +0800

    l

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Menu from './page/menu';
 import Title from './page/title'
-import './js/indexJS'
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import './js/indexJS';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { route } from 'express/lib/router';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Menu />
-        <Title />
-      </div>
-    </Router>
-  );
+    return ( 
+      <Router >
+        <div className = "App" >
+          <Menu/>
+          <Title/>
+        </div>
+      </Router >
+    );
 }
 
 export default App;
--- myapp/src/js/indexJS.js
+++ myapp/src/js/indexJS.js
@@ -1,21 +1,19 @@
 
-    let toggle =document.querySelector('.toggle');
-    let navigation =document.querySelector('.navigation');
-    let main =document.querySelector('.main');
-    toggle.onclick=function(){
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    toggle.onclick = function() {
         navigation.classList.toggle('active');
         main.classList.toggle('active')
     }
-
-
-
-
-//hoverer
-let list=document.querySelectorAll('.navigation li');
-function activeLink(){
-    list.forEach((item)=>
-    item.classList.remove('hovered'));
-    this.classList.add('hovered')
-}
-list.forEach((item=>
-item.addEventListener('mouseover',activeLink)))
+    
+    //hoverer
+    let list = document.querySelectorAll('.navigation li');
+    
+    function activeLink() {
+        list.forEach((item) =>
+            item.classList.remove('hovered'));
+        this.classList.add('hovered')
+    }
+    list.forEach((item =>
+        item.addEventListener('mouseover', activeLink)))
